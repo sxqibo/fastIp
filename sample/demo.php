be0c94366e881ede6ccb6c3f7dec72a0443bcce4
YangHongwei <403236160@qq.com>
2023-08-08 11:36:55 +0800

    增加自动引入

--- sample/demo.php
+++ sample/demo.php
@@ -2,5 +2,8 @@
 
 use Sxqibo\FastIp\PcOnline;
 
+require __DIR__ . '/../vendor/autoload.php';
+
 $obj = new PcOnline();
-$obj->getIpCity('39.144.96.133');
+$result = $obj->getIpCity('39.144.96.133');
+print_r($result);
